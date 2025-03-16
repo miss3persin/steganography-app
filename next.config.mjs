a9c41c5cc12700c800949897b10d40b31ba3fef7
Segun <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2025-03-16 21:28:25 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images.unsplash.com', 'source.unsplash.com'], // Add the domains you want to use for external images
+    },
+  };
+  
+  export default nextConfig;
+  
